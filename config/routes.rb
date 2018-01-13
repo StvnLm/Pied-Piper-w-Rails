ec6297bb20d59463f5f9cb670bfe892792ac7a4b
steven lam <ca.stevenlam@gmail.com>
2018-01-13 06:20:24 +0000

    added contact_mailer.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 Rails.application.routes.draw do
 	root to: 'pages#home'
 	get '/about', to: 'pages#about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :contacts
+    # resources :contacts
+    resources :contacts, only: :create
+    get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
