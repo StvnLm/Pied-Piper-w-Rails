90bc59546d419e7bc4982f7a2f876e9818c5152c
steven lam <ca.stevenlam@gmail.com>
2018-01-13 20:19:15 +0000

    changed default email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'ca.stevenlam@gmail.com'
+    default to: 'stevenlam5796@gmail.com'
     
     def contact_email(name, email, body)
        @name = name
