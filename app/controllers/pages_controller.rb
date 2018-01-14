b096d74a7d062ee953b9b4b186ac10f746607401
steven lam <ca.stevenlam@gmail.com>
2018-01-14 08:47:52 +0000

    Made plans page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
     def home
     end
     
+    def plans
+    end
+    
     def about
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # resources :contacts
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
+  get 'plans', to: 'pages#plans'
 end
