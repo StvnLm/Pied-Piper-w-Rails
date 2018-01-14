517fcd4cbda2719d3a04b137881539d19c367494
steven lam <ca.stevenlam@gmail.com>
2018-01-14 07:44:02 +0000

    Added link helpers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
 	root to: 'pages#home'
-	get '/about', to: 'pages#about'
+	get 'about', to: 'pages#about'
   # resources :contacts
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
