7dc3df18aaf401077dbbccfa59647d733c999e7e
steven lam <ca.stevenlam@gmail.com>
2018-01-15 05:09:22 +0000

    Added conditional logic on signup forms, and added fade in css on nav

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
+    # GET request for / (home)
     def home
     end
     
     def plans
+    # Instance variables are defined in controller and then can be used in views
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
     end
     
     def about
