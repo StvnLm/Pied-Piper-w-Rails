a6766eb18ecc6c36858a0fb2070f6276b7d733c5
steven lam <ca.stevenlam@gmail.com>
2018-01-20 08:25:27 +0000

    added code comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   
   # Whitelist form variable to be used
   attr_accessor :stripe_card_token
+  # If Pro user passes validations (email, password, etc), then call Stripe and 
+  # tell Stripe to set up a subscription upon charging the customers card token.
+  # Stripe responds back /w customer data. We store customer.ida s the customer 
+  # token and save the user.
   def save_with_subscription
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
